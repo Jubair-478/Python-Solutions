40e41aa72985b30e9d31ddf31f49cde573fb1c61
Jubair-478 <jujupy487@gmail.com>
2025-02-18 22:36:06 +0600

    Third version of my application

--- 01Variable_Swap.py
+++ 01Variable_Swap.py
@@ -8,3 +8,17 @@ for i in range(1, days+1):
     print(f"Days {i} save {Amount} Taka")
 
 print(Amount)
+
+
+
+
+Amount= 0
+
+days = int(input("Input the days you want to save money: "))
+
+for i in range(1, days+1):
+    Amount = Amount + i
+
+    print(f"Days {i} save {Amount} Taka")
+
+print(Amount)
