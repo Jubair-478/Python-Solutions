5521eb756508c0c46bfebc53773a2a4fbfe461d5
Jubair-478 <jujupy487@gmail.com>
2025-02-18 22:33:44 +0600

    Here is the second version my application

---
+++ 01Variable_Swap.py
@@ -0,0 +1,10 @@
+Amount= 0
+
+days = int(input("Input the days you want to save money: "))
+
+for i in range(1, days+1):
+    Amount = Amount + i
+
+    print(f"Days {i} save {Amount} Taka")
+
+print(Amount)
